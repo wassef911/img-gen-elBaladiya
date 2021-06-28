1aff5727d4c5c0fb5adcccc727fb83be62a9ebeb
wassef911 <1.wassef911@gmail.com>
2021-06-28 19:58:25 +0100

    init

---
+++ src/Event.js
@@ -0,0 +1,30 @@
+const Jimp = require("jimp");
+
+class Event {
+  constructor(img = "", logo = "", date = "") {
+    this.img = img;
+    this.logo = logo;
+    this.date = date;
+  }
+
+  async createConference() {
+    try {
+      // load background and logo
+      const img = await Jimp.read(this.img);
+      const logo = await Jimp.read(this.logo);
+      // load font
+      const font = await Jimp.loadFont(Jimp.FONT_SANS_32_WHITE);
+
+      // aad date and logo
+      img.print(font, 1175, 845, this.date);
+      img.blit(logo, 1420, 0);
+
+      // save
+      await img.writeAsync("assets/newEvent.png");
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+module.exports = Event;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+const Event = require("./Event");
+
+const E = new Event(
+  "assets/event.png",
+  "assets/logo.png",
+  "30 janvier 2020 - 20:20"
+);
+E.createConference();
